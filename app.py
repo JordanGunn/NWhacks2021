ddc962f05a62eb12ff63ab68b88cea265d49f953
nickluong97@outlook.com <nickluong97@outlook.com>
2021-01-09 14:53:58 -0800

    added api testing for itunes search

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
+import requests
+import json
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def home():
+    url = 'https://itunes.apple.com/search?term=guns+and+roses&limit=5'
+    data = requests.get(url)
+    response = data.json()
+    thing = response["results"][0]["trackName"]
+    return render_template("base.html", thing=thing)
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host="0.0.0.0")
